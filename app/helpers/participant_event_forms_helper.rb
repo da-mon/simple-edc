8f4d355b4d386c1eae86f320b89f1e125a6483a5
Tom Power <tpower21@gmail.com>
2016-03-15 16:37:13 +0000

    use helpers

--- app/helpers/participant_event_forms_helper.rb
+++ app/helpers/participant_event_forms_helper.rb
@@ -1,2 +1,5 @@
 module ParticipantEventFormsHelper
+  def back_to_participant(participant)
+    link_to 'Back to participant', participant_path(participant)
+  end
 end
--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -1,6 +1,18 @@
 module ParticipantsHelper
   def link_to_participant_event_form(event_form)
-    link_to event_form.form.name, new_participant_participant_event_form_path(@participant.id, {:event_form_id => event_form.id})
-    #= link_to event_form.form.name, participant_event_form_path(ParticipantEventForm.find_or_create_by(:participant_id => @participant.id, :event_form_id => event_form.id))
+    link_to event_form.form.name, participant_event_form_link(get_participant_event_form(event_form), event_form.id)
+  end
+
+  private
+  def get_participant_event_form(event_form)
+    ParticipantEventForm.where(:participant_id => @participant.id, :event_form_id => event_form.id).first
+  end
+
+  def participant_event_form_link(participant_event_form, event_form_id)
+    if participant_event_form
+      edit_participant_event_form_path(participant_event_form)
+    else
+      new_participant_participant_event_form_path(@participant.id, {:event_form_id => event_form_id})
+    end
   end
 end
