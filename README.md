21f107e9a1ffd03f2951d1aeb462a47f698361e9
Tom Power <tpower21@gmail.com>
2016-03-16 15:55:09 +0000

    more peff

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A simple user administered electronic data capture system using rails
+A very simple, user administered electronic data capture system using rails
--- app/helpers/participant_event_forms_helper.rb
+++ app/helpers/participant_event_forms_helper.rb
@@ -3,11 +3,24 @@ module ParticipantEventFormsHelper
     link_to 'Back to participant', participant_path(participant)
   end
 
-  def peff_input(field, pef)
-    field.code
+  def peff_input(field, i)
+    field_value_tag(field, get_name(i))
   end
 
-  def get_participant_event_form_field(field, pef)
+  def get_name(i)
+    'patient_event_form[patient_event_form_fields_attributes][' + i.to_s + '][field_value]'
+  end
+
+  def participant_event_form_field(field, pef)
     ParticipantEventFormField.where(:field_id => field.id, :participant_event_form_id => pef.id).first
+  end
+
+  def field_value_tag(field, name)
+    case field.field_type.name
+      when 'text'
+        text_field_tag name
+      when 'text_big'
+        text_area_tag name
+    end
   end
 end
